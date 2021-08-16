597544391bb12aca840e1d23f314ee36455588f6
traceannwhite <traceannwhite@gmail.com>
2021-08-16 13:42:30 -0500

    Added URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Display from "./Display";
 import Form from "./Form";
 
 function App() {
-  const url = "http://localhost:4500";
+  const url = "https://dogs-api-tw.herokuapp.com";
 
   const [dogs, setDogs] = useState([]);
 
